5e6212eacec8091e0916c0b1cb4ef776d0142451
Kushiro Taichi <k.taichi0315@gmail.com>
2019-04-21 18:40:46 +0900

    create backend project

---
+++ backend/todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def _str_(self):
+        return self.title
---
+++ backend/todo/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets          
+from .serializers import TodoSerializer      
+from .models import Todo               
+
+class TodoView(viewsets.ModelViewSet):      
+    serializer_class = TodoSerializer         
+    queryset = Todo.objects.all()
